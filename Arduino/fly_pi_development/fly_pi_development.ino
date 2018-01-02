9c2afe91f55df56c86390c0c40b5d5cec7255474
amchagas <andremaia.chagas@gmail.com>
2018-01-02 02:58:28 +0100

    cleaned up arduino code. Changed answer token and removed code related to matrix and unused ring functions

--- Arduino/fly_pi_development/fly_pi_development.ino
+++ Arduino/fly_pi_development/fly_pi_development.ino
@@ -68,46 +68,7 @@ float lowLimit = 13.0; //in Celsius
 
 //create function to control LED ring
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(ring_nPixels, RingPin, NEO_GRB + NEO_KHZ800);
-/*
-//create function to control matrix
-Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 
-//copntrol for long matrix
-//Adafruit_8x16matrix matrix = Adafruit_8x16matrix();
-//matrix stimuli
-static const uint8_t PROGMEM // MATRIX PICS
-matrixPattern1[] =
-{ B00001111,
-  B00001111,
-  B00001111,
-  B00001111,
-  B11110000,
-  B11110000,
-  B11110000,
-  B11110000
-},
-matrixPattern2[] =
-{ B11110000,
-  B11110000,
-  B11110000,
-  B11110000,
-  B00001111,
-  B00001111,
-  B00001111,
-  B00001111
-},
-
-matrixPattern4[] =
-{ B11111111,
-  B11111111,
-  B11111111,
-  B11111111,
-  B11111111,
-  B11111111,
-  B11111111,
-  B11111111
-};
-*/
 
 void setup()
 { //start serial port
@@ -128,16 +89,8 @@ void setup()
   //pinMode(peltierCoolPin2,OUTPUT);
   focusServo.attach(servoPin);
   pixels.begin();
+  Serial.println("<wtd>>");
 
-/*
-  // pass in the address for LED Matrix
-  matrix.begin(0x70);
-  matrix.clear();
-  matrix.setBrightness(0);
-  matrix.writeDisplay();
-  digitalWrite(servoOnPin, LOW);
-  //Serial.println("waited");
-  */
 }//end void setup
 
 void loop() {
@@ -159,7 +112,7 @@ void loop() {
 
     waitmils = term2.toInt();
     waiting(waitmils);
-    Serial.println("waited");
+    Serial.println("<wtd>>");
     }
 
   //check temp sensor
@@ -167,6 +120,7 @@ void loop() {
     {  temperature = checkTemp(tempSensorPin);
         if (term2.toInt()!=99){newTemp=term2.toInt();}
        Serial.println(temperature);
+       
        if (peltOn == 1) {
           HoldTemp(newTemp, tempSensorPin, peltierCoolPin1, peltierHeatPin1);
     }
@@ -180,8 +134,8 @@ void loop() {
       digitalWrite(RGBluePin, HIGH);
       peltOn = 0;
     }//end if temperature >=...
-    Serial.println("waited");
-  }//end if incomingData==99
+    Serial.println("<wtd>>");
+  }//end if "TEM"
 
   //***************SERVO******************////
   if (term1 == "SER") {
@@ -196,19 +150,19 @@ void loop() {
       delay(15);
       //correction = 0;
     }//else
-  Serial.println("waited");}//end servo
+  Serial.println("<wtd>>");}//end servo
 
   //////////////////////////////////////////////////
   //LED1
   if (term1 == "LD1") {
     if (term2.toInt()==1){digitalWrite(LED1Pin, HIGH);}
     if (term2.toInt() == 0) {digitalWrite(LED1Pin, LOW);}
-  Serial.println("waited");}
+  Serial.println("<wtd>>");}
   if (term1 == "LZ1"){
     digitalWrite(LED1Pin, HIGH);
     waiting(term2.toInt());
     digitalWrite(LED1Pin, LOW);
-    Serial.println("waited");  
+    Serial.println("<wtd>>");  
   }
 
 
@@ -216,86 +170,14 @@ void loop() {
   if (term1 == "LD2") {
     if (term2.toInt()==1){digitalWrite(LED2Pin, HIGH);}
     if (term2.toInt() == 0) {digitalWrite(LED2Pin, LOW);}
-  Serial.println("waited");} 
+  Serial.println("<wtd>>");} 
     if (term1 == "LZ2"){
     digitalWrite(LED2Pin, HIGH);
     waiting(term2.toInt());
     digitalWrite(LED2Pin, LOW);
-    Serial.println("waited");  
+    Serial.println("<wtd>>");  
   }
  
-/*
-  //MATRIX
-  if (term1 == "MAT"){
-    if (term2.toInt() == 0) { 
-      //matrix off
-      matrix.clear();
-      matrix.writeDisplay();// write changes to the display
-      } 
-    if (term2.toInt() == 1) {
-      // matrix pattern 1
-      matrix.clear();
-      matrix.drawBitmap(0, 0, matrixPattern1, 8, 8, LED_ON);
-      matrix.writeDisplay();// write changes to the display
-      } 
-
-    if (term2.toInt() == 2) {
-      //matrix pattern 2
-      matrix.clear();
-      matrix.drawBitmap(0, 0, matrixPattern2, 8, 8, LED_ON);
-      matrix.writeDisplay();
-      }
-
-    if (term2.toInt() == 3) {
-      //matrix pattern 3
-      for (int j = 0; j < 2 ; j = j + 1) {
-        for (int i = 0; i < 4; i = i + 1) {
-          matrix.clear();
-          if (i == 0) {
-            matrix.drawLine(0, 0, 0, 7, LED_ON);  matrix.drawLine(1, 0, 1, 7, LED_ON);
-            matrix.drawLine(4, 0, 4, 7, LED_ON);  matrix.drawLine(5, 0, 5, 7, LED_ON);
-            }
-          if (i == 1) {
-            matrix.drawLine(1, 0, 1, 7, LED_ON);  matrix.drawLine(2, 0, 2, 7, LED_ON);
-            matrix.drawLine(5, 0, 5, 7, LED_ON);  matrix.drawLine(6, 0, 6, 7, LED_ON);
-            }
-          if (i == 2) {
-            matrix.drawLine(2, 0, 2, 7, LED_ON);  matrix.drawLine(3, 0, 3, 7, LED_ON);
-            matrix.drawLine(6, 0, 6, 7, LED_ON);  matrix.drawLine(7, 0, 7, 7, LED_ON);
-            }
-          if (i == 3) {
-            matrix.drawLine(3, 0, 3, 7, LED_ON);  matrix.drawLine(4, 0, 4, 7, LED_ON);
-            matrix.drawLine(7, 0, 7, 7, LED_ON);  matrix.drawLine(0, 0, 0, 7, LED_ON);
-            }
-          matrix.writeDisplay(); // write changes to the display
-
-          time1 = millis();
-          time2 = time1;
-          while (time2 - time1 < 100) {time2 = millis();}//end while
-        }//end for i
-      }//end for j
-    }//end if incomingData==3
-    
-     if (term2.toInt() == 4) {
-      //matrix pattern 2
-      matrix.clear();
-      matrix.drawBitmap(0, 0, matrixPattern4, 8, 8, LED_ON);
-      matrix.drawBitmap(0, 8, matrixPattern4, 8, 8, LED_ON);
-      matrix.writeDisplay();
-      }
-  Serial.println("waited");}//end if term1 == MAT
-  
-  if (term1=="MAB") {
-    //set brightness of the matrix
-    if (term2.toInt() == 0) {
-      matrix.clear();
-      matrix.writeDisplay();}
-    else {
-      matrix.setBrightness(term2.toInt());
-      matrix.writeDisplay();}
-    Serial.println("waited");}
-
-*/
   
   //RING
   if (term1=="RIN"){
@@ -308,21 +190,9 @@ void loop() {
       ringOn = 0;
       updateRing(0, 0, 0);
       pixels.show();}
-  Serial.println("waited");}//end if "RIN"
+  Serial.println("<wtd>>");}//end if "RIN"
 
 
-//  if (term1=="G"){
-//    //oldRed = ringRedHue;
-//    ringGreenHue=term2.toInt();
-//    updateRing(ringRedHue, ringGreenHue, ringBlueHue);
-//    pixels.show();
-//  }//end if term1=="G"
-//   if (term1=="B"){
-//    //oldRed = ringRedHue;
-//    ringBlueHue=term2.toInt();
-//    updateRing(ringRedHue, ringGreenHue, ringBlueHue);
-//    pixels.show();
-//  }//end if term1=="G"
   
   if (term1=="RRE"){
     //oldRed = ringRedHue;
@@ -332,7 +202,7 @@ void loop() {
       //if the ring is on
       //show the update
       pixels.show();}//end if ringOn==1
-  Serial.println("waited");}//end if term1=="RRE"
+  Serial.println("<wtd>>");}//end if term1=="RRE"
 
 
   if (term1 == "RGR") { //ring green
@@ -342,7 +212,7 @@ void loop() {
     updateRing(ringRedHue, ringGreenHue, ringBlueHue);
     if (ringOn == 1) {
       pixels.show();}
-  Serial.println("waited");}//end if term1== "RGR"
+  Serial.println("<wtd>>");}//end if term1== "RGR"
 
   if (term1 == "RBL") { //ring blue
     //oldBlue = ringBlueHue;
@@ -350,7 +220,7 @@ void loop() {
     updateRing(ringRedHue, ringGreenHue, ringBlueHue);
     if (ringOn == 1) {pixels.show();}
 
-  Serial.println("waited");}//end if RBL
+  Serial.println("<wtd>>");}//end if RBL
 
   if (term1 == "RAL") { //ring all together
     ringBlueHue = term2.toInt();
@@ -360,11 +230,11 @@ void loop() {
     if (ringOn == 1) {
       pixels.show();
     }
-  Serial.println("waited");}
+  Serial.println("<wtd>>");}
 
-  if (term1=="RZAR") {zapRed = term2.toInt();Serial.println("waited");}
-  if (term1=="RZAG") {zapGreen = term2.toInt();Serial.println("waited");}
-  if (term1=="RZAB") {zapBlue = term2.toInt();Serial.println("waited");}
+  if (term1=="RZAR") {zapRed = term2.toInt();Serial.println("<wtd>>");}
+  if (term1=="RZAG") {zapGreen = term2.toInt();Serial.println("<wtd>>");}
+  if (term1=="RZAB") {zapBlue = term2.toInt();Serial.println("<wtd>>");}
   if (term1=="RZAT") {    
     if (ringOn == 1) {
       updateRing(zapRed, zapGreen, zapBlue);
@@ -373,67 +243,9 @@ void loop() {
       updateRing(ringRedHue, ringGreenHue, ringBlueHue);
       pixels.show();      
     }//end if ring on
-  Serial.println("waited");}
-      //zapRed = pow(2,zapRed);
-//      zapGreen = term2.substring(4,7);
-//      zapBlue = term2.substring(2,3);
+  Serial.println("<wtd>>");}
     
-    
-
-
-  
-
-  if (term1 == "RRT") { //ring rotation
-    time1 = millis();
-    time2 = time1;
-    while (time2 - time1 < 1000) {
-      time2 = millis();
-      if (term2.toInt() > 500) { //check to see which side to turn
-        for (int i = 0; i < ring_nPixels; i = i + 2) {
-          pixels.setPixelColor(i, pixels.Color(ringRedHue, ringGreenHue, ringBlueHue));
-          if (i < ring_nPixels) {
-            pixels.setPixelColor(i + 1, pixels.Color(0, 0, 0)); //end if
-          }
-
-        }//end for
-        pixels.show();//update ring hardware
-        delay(term2.toInt() - 450);
-        for (int i = 0; i < ring_nPixels; i = i + 2) {
-          pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-          if (i < ring_nPixels) {
-            pixels.setPixelColor(i + 1, pixels.Color(ringRedHue, ringGreenHue, ringBlueHue)); //end if
-          }
-        }//end for
-        pixels.show();//update ring hardware
-        delay(term2.toInt() - 450);
-      }//end if correction>500
-
-      if (term2.toInt() < 500) {
-        for (int i = ring_nPixels; i >= 0; i = i - 2) {
-          pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-          if (i > 0) {
-            pixels.setPixelColor(i + 1, pixels.Color(ringRedHue, ringGreenHue, ringBlueHue)); //end if
-          }
-
-        }//end for
-        pixels.show();//update ring hardware
-
-        delay(term2.toInt() - 350);
-        for (int i = ring_nPixels; i >= 0; i = i - 2) {
-          pixels.setPixelColor(i, pixels.Color(ringRedHue, ringGreenHue, ringBlueHue));
-          if (i > 0) {
-            pixels.setPixelColor(i + 1, pixels.Color(0, 0, 0)); //end if
-          }
-        }//end for
-        pixels.show();//update ring hardware
-        delay(term2.toInt() - 350);
-      }//end if correction<500
-      //correction = 0;
-    }//end while
-
-
-  Serial.println("waited");
-  }//end if address==RRT
+   
 
   //PELTIER
   if (term1 == "PEL") { //Peltier on
@@ -453,10 +265,10 @@ void loop() {
     digitalWrite(RGBluePin, LOW);
     peltOn = 0;
   }
-  Serial.println("waited");}
+  Serial.println("<wtd>>");}
   
   if (term1 == "PET") {newTemp = term2.toInt();
-  Serial.println("waited");}
+  Serial.println("<wtd>>");}
 term1=String("0");
 term2=String("0");
 }//end void loop
@@ -494,6 +306,11 @@ float checkTemp(int pinToRead) {
 float HoldTemp(float finalTemp, int tempSensorPin,
                int peltierCoolPin1, int peltierHeatPin1) {
   float temperature, temps;
+
+
+  
+
+  
   temperature = checkTemp(tempSensorPin);
   //Serial.print("temp: ");
   //Serial.println(temperature);
@@ -515,17 +332,19 @@ float HoldTemp(float finalTemp, int tempSensorPin,
     analogWrite(RGreenBPin, 0);
     analogWrite(RGBluePin, 255);
   }//end if temperature > finaTemp
+  
   if ((temperature <= (finalTemp + TempTol)) && (temperature >= (finalTemp - TempTol))) {
     //digitalWrite(peltierEnablePin,LOW);
     //Serial.println("temp reached!");
+
     digitalWrite(peltierHeatPin1, LOW);
     digitalWrite(peltierCoolPin1, LOW);
     analogWrite(RGreenBPin, 255);
     analogWrite(RedGBPin, 0);
     analogWrite(RGBluePin, 0);
+    
   }// end if temperature < finalTemp && temperature>finalTemp
-  //if (finalTemp<temperature) { analogWrite(RedGBPin,50); }
-  //if (finalTemp>temperature) { analogWrite(RGBluePin,50); }
+
 
 
 }
@@ -555,7 +374,7 @@ void waiting(int millistowait) {
   
   //handshake
 //  while(flag==1){
-//    Serial.println("waited");
+//    Serial.println("<wtd>>");
 //    token1=Serial.readStringUntil('>');
 //    index = token.indexOf('<');
 //    if (token.substring(0,index)=="END"){flag=0;}
